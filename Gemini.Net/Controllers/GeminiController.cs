5b8c24bb8f9cdaa77b9ef59850c87bed10963e0b
FabriciusFelix <fabricius.felix@yahoo.com.br>
2024-04-11 00:50:55 -0300

    First commit

---
+++ Gemini.Net/Controllers/GeminiController.cs
@@ -0,0 +1,38 @@
+﻿using Gemini.Net.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gemini.Net.Controllers
+{
+    public class GeminiController : Controller
+    {
+        // GET: GeminiController
+        public IActionResult Index()
+        {
+            return View();
+        }
+ 
+        public IActionResult EnviaPromptPage()
+        {
+            return View();
+        }
+
+         
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult PostPrompt(IFormCollection collection)
+        {
+            var prompt = new Gemini.Net.Models.Gemini(1,"Quem é você?");
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(EnviaPromptPage());
+            }
+        }
+
+         
+    }
+}
---
+++ Gemini.Net/Models/Gemini.cs
@@ -0,0 +1,18 @@
+﻿namespace Gemini.Net.Models
+{
+
+    public class Gemini
+    {
+        public Gemini(int id, string prompt)
+        {
+            Id = id;
+            Prompt = prompt;
+        }
+
+        public int Id { get; set; }
+
+
+        public string Prompt { get; set; }
+    }
+
+}
